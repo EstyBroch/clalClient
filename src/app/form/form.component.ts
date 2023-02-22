066458402982edbd8a2c892cb2d431c62832fe92
esty broch <brochesty@gmail.com>
2023-02-22 15:21:19 +0200

    second

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -10,25 +10,11 @@ import { WeatherService } from './../services/weather.service'
 export class FormComponent {
   constructor(private weatherService: WeatherService) { }
 
-  weather: any = "";
-  tempC:string="";
-  weatherArr:WeatherModel[]=[]
-  weatherM:WeatherModel={ date:Date.toString(),
-    avgtemp_C:"",
-    avgvis:""}
-  // ngOnInit(): void {
-  //   this.getWeather("");
-  // }
-
+  weather: string = "";
+  
   getWeather(location:string) {
     this.weatherService.getWeather(location).subscribe(res => {
-      this.weather = res['forecast'];
-      let a = res.forecast;
-      this.tempC=res[0]
-    // let forecast =this.weather['forecast'];
-     this.tempC=JSON.stringify(res, null, 4) ;
-    // this.weatherM.avgtemp_C=res.
-      //this.tempC=res.lo
+      this.weather = res;
     });
   }
 
