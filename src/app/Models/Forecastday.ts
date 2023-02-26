c085a9929b516a253847af6e33d8b10fa0614edb
esty broch <brochesty@gmail.com>
2023-02-26 17:35:51 +0200

    main

--- src/app/Models/Forecastday.ts
+++ src/app/Models/Forecastday.ts
@@ -1,5 +1,7 @@
 import { Day } from "./Day"
+import { Hour } from "./Hour"
 export interface Forecastday{
     date:string,
     day:Day
+    hour:Hour[]
 }
---
+++ src/app/Models/Hour.ts
@@ -0,0 +1,6 @@
+
+export interface Hour {
+    time: string
+    temp_c: string
+    temp_f: string
+}
